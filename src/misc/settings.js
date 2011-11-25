356ca90bc4a1ac523f7669c22973e75502254d15
Richard Duivenvoorde <richard@duif.net>
2011-11-25 11:57:14 +0100

    oops, accidently put test. back again, removing it now

--- src/misc/settings.js
+++ src/misc/settings.js
@@ -12,9 +12,9 @@ OpenLayers.IMAGE_RELOAD_ATTEMPTS = 2;
 OpenLayers.ProxyHost = 'proxy.jsp?';
 
 // urls
-Geozet.config.bekendmakingenWFS = 'http://test.geodata.nationaalgeoregister.nl/pdok/wfs?';
-Geozet.config.WMSC = 'http://test.geodata.nationaalgeoregister.nl/wmsc?';
-Geozet.config.gazetteer = {url:"http://test.geodata.nationaalgeoregister.nl/geocoder/Geocoder?", param:"zoekterm"};
+Geozet.config.bekendmakingenWFS = 'http://geodata.nationaalgeoregister.nl/pdok/wfs?';
+Geozet.config.WMSC = 'http://geodata.nationaalgeoregister.nl/wmsc?';
+Geozet.config.gazetteer = {url:"http://geodata.nationaalgeoregister.nl/geocoder/Geocoder?", param:"zoekterm"};
 Geozet.config.vlakgerichteBekendmakingenService = "vlakbekendmakingen?";
 
 OpenLayers.Lang.nl = OpenLayers.Util.extend(
